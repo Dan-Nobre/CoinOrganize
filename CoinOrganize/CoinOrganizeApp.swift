083c4cf4d6319357809786ad3f4eef58a9f6af14
Daniel Nobre <55868687+Dan-Nobre@users.noreply.github.com>
2025-08-30 18:54:54 -0300

    Initial Commit

---
+++ CoinOrganize/CoinOrganizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinOrganizeApp.swift
+//  CoinOrganize
+//
+//  Created by Daniel Nobre on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinOrganizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
